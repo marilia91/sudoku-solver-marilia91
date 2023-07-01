24d0c23c93bd9ace492f2080cecb513ceab0df5e
marilia91 <marilia.iza112@gmail.com>
2023-06-30 21:31:22 -0300

    exercício resolvido

--- main.py
+++ main.py
@@ -1,49 +1,47 @@
-def solve_sudoku(board: list[list[int]]) -> list[list[int]]:
-    def resolve(board: list[list[int]]) -> bool:
-        i, j = célula_vazia(board)
+def solve_sudoku(boards: list[list[int]]) -> list[list[int]]:
+    def resolve(boards: list[list[int]]) -> bool:
+        i, j = Board(boards)
         if i == None or j == None:
             return True 
 
         for num in range(1, 10):
-            board[i][j] = num 
+            boards[i][j] = num 
 
-            if is_valid(board) and resolve(board): 
+            if is_valid(boards) and resolve(boards): 
                 return True
 
-            board[i][j] = 0 
+            boards[i][j] = 0 
 
         return False
 
-    if resolve(board):
-        return board
-    else:
-        raise ValueError
+    if resolve(boards):
+        return boards
+    raise ValueError
 
-def is_valid(board: list[list[int]]) -> bool:
+def is_valid(boards: list[list[int]]) -> bool:
     for i in range(9):
         for j in range(9):
-            num = board[i][j]
+            num = boards[i][j]
             if num != 0:
                 for col in range(9):
-                    if col != j and board[i][col] == num:
+                    if col != j and boards[i][col] == num:
                         return False
 
                 for row in range(9):
-                    if row != i and board[row][j] == num:
+                    if row != i and boards[row][j] == num:
                         return False
                 start_row = 3 * (i // 3) 
                 start_col = 3 * (j // 3)
                 for row in range(start_row, start_row + 3):
                     for col in range(start_col, start_col + 3):
-                        if (row != i or col != j) and board[row][col] == num:
+                        if (row != i or col != j) and boards[row][col] == num:
                             return False
 
     return True
 
-def célula_vazia(board: list[list[int]]) -> tuple[int, int]:
-
+def Board(boards: list[list[int]]) -> tuple[int, int]:
     for i in range(9):
         for j in range(9):
-            if board[i][j] == 0:
+            if boards[i][j] == 0:
                 return i, j
     return None, None
